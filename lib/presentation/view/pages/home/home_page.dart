219a6a6a30e6efe0ce3ea53d9244d5cea0eca0a0
Bek <nematjanov95@gmail.com>
2023-04-06 19:17:57 +0500

    UI fixing

--- lib/presentation/view/pages/home/home_page.dart
+++ lib/presentation/view/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:setup_provider/presentation/style/style.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,13 +9,45 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-      child: Column(
-        children: [
-          Text(
-            'Restaurants',
-            style: Style.bold(size: 20, color: Style.black),
-          )
-        ],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Restaurants',
+              style: Style.bold(size: 20, color: Style.black),
+            ),
+            12.verticalSpace,
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: 1,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    height: 248.h,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Style.black,
+                        borderRadius: BorderRadius.circular(24)),
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 118.h,
+                          decoration: const BoxDecoration(
+                              color: Style.primaryColor,
+                              borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(24),
+                                  topRight: Radius.circular(24))),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     ));
   }
