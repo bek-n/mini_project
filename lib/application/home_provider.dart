76fa05b6a4658f14dde85a41bbfa099d80a1a1c9
Bek <nematjanov95@gmail.com>
2023-04-07 00:11:36 +0500

    fixing

--- lib/application/home_provider.dart
+++ lib/application/home_provider.dart
@@ -1,36 +1,27 @@
 import 'package:flutter/cupertino.dart';
-import 'package:setup_provider/domain/facade/auth_facade.dart';
 import 'package:setup_provider/domain/facade/home_facade.dart';
 import 'package:setup_provider/infastructura/servises/app_helper.dart';
-import 'package:setup_provider/infastructura/servises/local_store.dart';
 
 
 class AuthProvider extends ChangeNotifier {
-  final AuthFacade authRepo;
   final HomeFacade homeRepo;
   bool isLoading = false;
 
-  AuthProvider(this.authRepo, this.homeRepo);
+  AuthProvider(this.homeRepo);
 
   login(
       {required BuildContext context, required VoidCallback onSuccess}) async {
     isLoading = true;
     notifyListeners();
-    final res = await authRepo.login("email");
+    final res = await homeRepo.getRestaurants();
     res.fold((data) {
       isLoading = false;
       notifyListeners();
       onSuccess();
-      LocalStore.setToken(data.firstname);
     }, (error) {
       isLoading = false;
       notifyListeners();
-      AppHelper.errorSnackBar(context: context,message: error);
+      AppHelper.errorSnackBar(context: context, message: error);
     });
-  }
-
-  logOut({required VoidCallback onSuccess}) async {
-    LocalStore.clear();
-    onSuccess();
   }
 }
