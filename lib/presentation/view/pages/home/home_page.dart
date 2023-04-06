1572460bcc061f15bb2a617b491142f4e5fc29b7
Bek <nematjanov95@gmail.com>
2023-04-06 19:28:47 +0500

    fixing

--- lib/presentation/view/pages/home/home_page.dart
+++ lib/presentation/view/pages/home/home_page.dart
@@ -2,9 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:setup_provider/presentation/style/style.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+   
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
